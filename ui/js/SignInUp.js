eb2c3145377ee7a157c06ea8075b3ea767c64815
gwyrwch <gwyrwch@gmail.com>
2020-11-15 18:59:03 +0300

    sign in form

--- ui/js/SignInUp.js
+++ ui/js/SignInUp.js
@@ -5,6 +5,36 @@ export default class SignInUp extends Page {
     constructor() {
         super();
 
+        let self = this;
+
+        self.setSignInUpButtonsOnclick();
+    }
+
+    setSignInUpButtonsOnclick() {
+        let signInForm = document.getElementsByClassName('sign-in-form').item(0);
+        let signUpForm = document.getElementsByClassName('sign-up-form').item(0);
+
+        let signInButton = document.getElementsByClassName('sign-in-button').item(0);
+        let signUpButton = document.getElementsByClassName('sign-up-button').item(0);
+
+        signInButton.onclick = function() {
+            this.classList.add('selected-sign-in-up-button', 'selected-sign-in-button');
+            signInForm.style.display = 'block';
+
+            signUpButton.classList.remove('selected-sign-in-up-button', 'selected-sign-up-button');
+            signUpForm.style.display = 'none';
+        }
+
+        signUpButton.onclick = function() {
+            this.classList.add('selected-sign-in-up-button', 'selected-sign-up-button');
+            signUpForm.style.display = 'block';
+
+            signInButton.classList.remove('selected-sign-in-up-button', 'selected-sign-in-button');
+            signInForm.style.display = 'none';
+        }
+
+
+
     }
 
     static getHtml() {
