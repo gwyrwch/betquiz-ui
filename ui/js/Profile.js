de24f4ec3622c6aabe6767f7c603aeea63c8a174
gwyrwch <gwyrwch@gmail.com>
2020-11-19 15:31:36 +0300

    user statistics design

--- ui/js/Profile.js
+++ ui/js/Profile.js
@@ -32,6 +32,19 @@ export default class Profile extends Page {
                         self.setPaymentInformationButtonOnclick();
                         self.setLogoutButtonOnclick();
                         self.setSettingsButtonOnclick();
+
+                        new Chartist.Line('.ct-chart', {
+                            labels: [1, 2, 3, 4, 5, 6, 7, 8],
+                            series: [
+                                [5, 9, 7, 8, 5, 3, 5, 4]
+                            ]
+                        }, {
+                            // low: 0,
+                            showArea: true,
+                            width: '60vw',
+                            height: '48vh'
+                        });
+
                     }
                 });
             } else {
